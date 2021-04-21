af4bf7ccecdd61a98f8391cb6bf480044d457df4
Giuseppe Barbieri <elect86@gmail.com>
2021-04-21 20:28:08 +0200

    up

--- build-src/settings.gradle.kts
+++ build-src/settings.gradle.kts
@@ -2,7 +2,7 @@
 dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
-            create("foo").to("group:artifact:0.0.1")
+            alias("foo").to("group:artifact:0.0.1")
         }
     }
 }
